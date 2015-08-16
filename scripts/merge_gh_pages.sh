9d1337e70a5b7def0f1983c422ed32ce5ef66bab
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-08-16 22:08:11 +0300

    Scripts: Yet another deploy fix

--- scripts/merge_gh_pages.sh
+++ scripts/merge_gh_pages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-set -pipefail
+#!/bin/bash
+set -o pipefail
 export GIT_COMMITTER_NAME=APIs-GuruBot
 git push "https://${GH_TOKEN}@${GH_REF}" "${TRAVIS_COMMIT}:gh-pages" 2>&1 | sed "s/${GH_TOKEN}/xxPASSxx/"
