6e317c5397a4ab824e898aab0347109131ef80ef
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-08-16 22:12:57 +0300

    Scripts: debug commit

--- scripts/merge_gh_pages.sh
+++ scripts/merge_gh_pages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -o pipefail
 export GIT_COMMITTER_NAME=APIs-GuruBot
-git push "https://${GH_TOKEN}@${GH_REF}" "${TRAVIS_COMMIT}:gh-pages" 2>&1 | sed "s/${GH_TOKEN}/xxPASSxx/"
+git push "https://${GH_TOKEN}@${GH_REF}" "${TRAVIS_COMMIT}:gh-pages" 2>&1
+#| sed "s/${GH_TOKEN}/xxPASSxx/"
