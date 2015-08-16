c64fd2cf204b57cb4a9dcef15e78e3827980dc9c
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-08-16 21:07:35 +0300

    Scripts: fix repo name

--- scripts/merge_gh_pages.sh
+++ scripts/merge_gh_pages.sh
@@ -2,7 +2,7 @@
 set -e
 
 export GIT_COMMITTER_NAME=APIs-GuruBot
-git checkout gh_pages
+git checkout gh-pages
 git merge "$TRAVIS_COMMIT"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"
 git checkout master
