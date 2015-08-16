42d4b3d468404f70b4bfd5a15d6bd0748d1f1f09
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-08-16 21:50:40 +0300

    Scripts: fix incorect sed rule

--- scripts/merge_gh_pages.sh
+++ scripts/merge_gh_pages.sh
@@ -2,4 +2,4 @@
 set -e
 
 export GIT_COMMITTER_NAME=APIs-GuruBot
-git push "https://$GH_TOKEN@$GH_REF" master:gh-pages 2>&1 | sed "s/$GH_REF/xxPASSxx/"
+git push "https://$GH_TOKEN@$GH_REF" master:gh-pages 2>&1 | sed "s/$GH_TOKEN/xxPASSxx/"
