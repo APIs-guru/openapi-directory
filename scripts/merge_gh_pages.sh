6c099d9996d2a15094421627157cb84b2328c1f9
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-08-16 21:26:18 +0300

    Scripts: checkout branch correctly

--- scripts/merge_gh_pages.sh
+++ scripts/merge_gh_pages.sh
@@ -2,7 +2,7 @@
 set -e
 
 export GIT_COMMITTER_NAME=APIs-GuruBot
-git checkout gh-pages
+git checkout -b gh-pages origin/gh-pages
 git merge "$TRAVIS_COMMIT"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"
 git checkout master
