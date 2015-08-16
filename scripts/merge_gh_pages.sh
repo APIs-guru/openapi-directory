d93b3763bbb127df5edb2247a1a921697dde060d
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-08-16 21:44:32 +0300

    Scripts: deploy directly to remote branch

--- scripts/merge_gh_pages.sh
+++ scripts/merge_gh_pages.sh
@@ -2,7 +2,4 @@
 set -e
 
 export GIT_COMMITTER_NAME=APIs-GuruBot
-git checkout -b gh-pages origin/gh-pages
-git merge "$TRAVIS_COMMIT"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"
-git checkout master
+git push "https://$GH_TOKEN@$GH_REF" master:gh-pages 2>&1 | sed "s/$GH_REF/xxPASSxx/"
