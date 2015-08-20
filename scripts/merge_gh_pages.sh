cc6f363c0615851a380fa66cbec38929d33bf963
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-08-20 15:11:18 +0300

    Scripts: hide GH token in log files

--- scripts/merge_gh_pages.sh
+++ scripts/merge_gh_pages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -o pipefail
+set -x
 export GIT_COMMITTER_NAME=APIs-GuruBot
 ./scripts/gen_apilist.sh
 git add ./apilist.json
 git commit -m 'Generate apilist.json'
 git push --force "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages 2>&1
-#| sed "s/${GH_TOKEN}/xxPASSxx/"
