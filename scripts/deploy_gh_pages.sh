7b8c829f3f08989d925e87b9c76e38325c2d1619
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-08-29 00:57:44 +0300

    Scripts: error if deploy failed. Part 2

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -e
-set -x
-export GIT_COMMITTER_NAME=APIs-GuruBot
-./scripts/gen_apilist.sh
-./scripts/collection.js api https://apis-guru.github.io/api-models/
-git add ./apilist.json ./api/v1/list.json
-git commit -m 'Generate API'
-git push --force "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages 2>&1
+set -o pipefail
+(
+  set -e
+  set -x
+  export GIT_COMMITTER_NAME=APIs-GuruBot
+  ./scripts/gen_apilist.sh
+  ./scripts/collection.js api https://apis-guru.github.io/api-models/
+  git add ./apilist.json ./api/v1/list.json
+  git commit -m 'Generate API'
+  git push --force "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages 2>&1
+) 2>&1 | sed "s/${GH_TOKEN}/xxPASSxx/"
