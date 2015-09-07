8975e62cdbda94bdda1834359469f36a69c60cf5
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-09-07 22:02:35 +0300

    Scripts: clean files after deploy.

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -9,4 +9,5 @@ set -o pipefail
   git add ./api/v1/list.json
   git commit -m 'Generate API'
   git push --force "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages 2>&1
+  git clean -d
 ) 2>&1 | sed "s/${GH_TOKEN}/xxPASSxx/"
