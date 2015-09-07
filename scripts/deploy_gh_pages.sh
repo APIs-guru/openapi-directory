12c7378abec1427fae4ca2352e513829ddd999c3
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-09-07 22:01:18 +0300

    Scripts: remove pre v1 API. Part 2

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -6,7 +6,7 @@ set -o pipefail
   export GIT_COMMITTER_NAME=APIs-GuruBot
   npm install
   ./scripts/collection.js api https://apis-guru.github.io/api-models/
-  git add ./apilist.json ./api/v1/list.json
+  git add ./api/v1/list.json
   git commit -m 'Generate API'
   git push --force "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages 2>&1
 ) 2>&1 | sed "s/${GH_TOKEN}/xxPASSxx/"
