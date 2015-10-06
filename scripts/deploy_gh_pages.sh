8083ae18b37b5312ed6d749890ca2d31bbeb7bca
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2015-10-07 02:01:10 +0300

    Scripts: better commit message for "APIs-GuruBot"

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -3,11 +3,11 @@ set -o pipefail
 (
   set -e
   set -x
-  export GIT_COMMITTER_NAME=APIs-GuruBot
-  export GIT_COMMITTER_EMAIL=founders@APIs.guru
   npm install
   ./scripts/collection.js api https://apis-guru.github.io/api-models/
   git add ./api/v1/list.json
+  git config user.name "APIs-GuruBot"
+  git config user.email "founders@APIs.guru"
   git commit -m 'Generate API'
   git push --force "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages
   git clean -xdf
