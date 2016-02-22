38a454bdf292756ed7cc3f89c4019e28f1c0b758
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2016-02-22 18:37:11 +0200

    Scripts: fix function name

--- scripts/collection.js
+++ scripts/collection.js
@@ -325,7 +325,7 @@ function generateCSV(list) {
 
 function generateAPIsJSON(specRootUrl) {
   var apisJsonPath = __dirname + '/apis.json'
-  var collection = _.extend(readJSON(apisJsonPath), {
+  var collection = _.extend(readJson(apisJsonPath), {
     added: '2015-10-15',
     modified: new Date().toISOString().substring(0, 10),
     url: specRootUrl + 'apis.json',
