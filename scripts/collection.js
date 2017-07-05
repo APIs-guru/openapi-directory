84235fb2b30f89656217c009706c8ff730aad4ea
Mike Ralphson <mike.ralphson@gmail.com>
2017-07-05 22:35:22 +0100

    Fix bug with tmp.yaml output

--- scripts/collection.js
+++ scripts/collection.js
@@ -855,10 +855,10 @@ function errorToString(error, context) {
   }
 
   result += '???????????????????? Swagger ' + source + ' ????????????????????????????\n';
-  if (!_.isUndefined(swagger))
+  if (!_.isUndefined(swagger)) {
     result += util.Yaml2String(swagger);
-
-  fs.writeFileSync('../tmp.yaml',util.Yaml2String(swagger),'utf8');
+    fs.writeFileSync('../tmp.yaml',util.Yaml2String(swagger),'utf8');
+  }
 
   result += '!!!!!!!!!!!!!!!!!!!! Errors ' + source + ' !!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n';
   if (_.get(validation, 'errors'))
