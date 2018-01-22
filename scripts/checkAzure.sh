fcff2669b46e3fc5ac9110c9e77ebc80d970bdfb
Mike Ralphson <mike.ralphson@gmail.com>
2018-01-22 15:46:03 +0000

    scripts; widen checkAzure domains

--- scripts/checkAzure.sh
+++ scripts/checkAzure.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 find azure.com -name swagger.yaml | xargs grep -l '\$ref: .\.'
-find azure.com -name swagger.yaml | xargs grep -l '\$ref: .http'
+find azure.com -name swagger.yaml | xargs grep -l '\$ref: .\.'
+find microsoft.com -name swagger.yaml | xargs grep -l '\$ref: .\.'
+find microsoft.com -name swagger.yaml | xargs grep -l '\$ref: .\.'
+find windows.net -name swagger.yaml | xargs grep -l '\$ref: .http'
+find windows.net -name swagger.yaml | xargs grep -l '\$ref: .http'
